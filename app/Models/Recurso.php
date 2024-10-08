ea7e9baf60e6444ad8bc9af2a19e07ee19b76bd9
EdeRemastering <146907295+EdeRemastering@users.noreply.github.com>
2024-10-08 17:23:42 -0500

    He agregado los modelos correspondientes a mi módulo
    He agregado los modelos correspondientes a mi módulo ambientes, recursos, novedades

---
+++ app/Models/Recurso.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Recurso extends Model
+{
+    use HasFactory;
+
+    protected $table = 'recurso';
+    protected $primaryKey = 'id_recurso';
+    public $timestamps = false;
+
+    protected $fillable = ['id_ambiente', 'descripcion', 'estado'];
+}
